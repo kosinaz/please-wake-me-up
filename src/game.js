ff3586dbc779d578691b0f061d2e51190d2c93ed
kosinaz <kosinaz@gmail.com>
2020-08-28 21:27:44 +0200

    add own tileset and spritesheet and animate player

--- src/game.js
+++ src/game.js
@@ -43,7 +43,7 @@ class Game {
   create([playerImage]) {
     // eslint-disable-next-line new-cap
     this.map = TileEngine(dataAssets['data/map']);
-    this.player = new Player(12, 12, playerImage, this.map);
+    this.player = new Player(15, 21, playerImage, this.map);
     this.loop.start(); // start the game
   }
 
--- src/player.js
+++ src/player.js
@@ -1,8 +1,10 @@
 import {
   Sprite,
+  SpriteSheet,
   initKeys,
   bindKeys,
   keyPressed,
+  degToRad,
 } from '/lib/kontra.min.mjs';
 /**
  * Represents the player character.
@@ -23,10 +25,27 @@ export default class Player {
     this.x = x;
     this.y = y;
     // eslint-disable-next-line new-cap
-    this.sprite = Sprite({
-      x: x * 16, // starting x,y position of the sprite
-      y: y * 16,
+    const spriteSheet = SpriteSheet({
       image: image,
+      frameWidth: 16,
+      frameHeight: 16,
+      animations: {
+        idle: {
+          frames: 2,
+          loop: false,
+        },
+        walk: {
+          frames: [2, 1, 0, 1, 2, 3, 4, 3],
+          frameRate: 30,
+        },
+      },
+    });
+    // eslint-disable-next-line new-cap
+    this.sprite = Sprite({
+      x: x * 16 + 8, // starting x,y position of the sprite
+      y: y * 16 + 8,
+      anchor: {x: 0.5, y: 0.5},
+      animations: spriteSheet.animations,
     });
     this.map = map;
     initKeys();
@@ -43,29 +62,35 @@ export default class Player {
   update() {
     if (!this.moving) {
       if (keyPressed('left')) {
+        this.sprite.rotation = degToRad(90);
         this.moveTo(this.x - 1, this.y);
       } else if (keyPressed('right')) {
+        this.sprite.rotation = degToRad(-90);
         this.moveTo(this.x + 1, this.y);
       }
     }
     if (!this.moving) {
       if (keyPressed('up')) {
+        this.sprite.rotation = degToRad(180);
         this.moveTo(this.x, this.y - 1);
       } else if (keyPressed('down')) {
+        this.sprite.rotation = degToRad(0);
         this.moveTo(this.x, this.y + 1);
       }
     }
-    if (this.sprite.x < this.x * 16) {
+    if (this.sprite.x < this.x * 16 + 8) {
       this.sprite.x += 2;
-    } else if (this.sprite.x > this.x * 16) {
+    } else if (this.sprite.x > this.x * 16 + 8) {
       this.sprite.x -= 2;
     }
-    if (this.sprite.y < this.y * 16) {
+    if (this.sprite.y < this.y * 16 + 8) {
       this.sprite.y += 2;
-    } else if (this.sprite.y > this.y * 16) {
+    } else if (this.sprite.y > this.y * 16 + 8) {
       this.sprite.y -= 2;
     }
-    if (this.sprite.x === this.x * 16 & this.sprite.y === this.y * 16) {
+    if (this.sprite.x === this.x * 16 + 8 &&
+        this.sprite.y === this.y * 16 + 8) {
+      this.sprite.playAnimation('idle');
       this.moving = false;
     }
     this.sprite.update();
@@ -88,7 +113,9 @@ export default class Player {
    * @memberof Player
    */
   moveTo(x, y) {
-    if (this.map.tileAtLayer('layer', {col: x, row: y}) !== 35) {
+    const tileID = this.map.tileAtLayer('layer', {col: x, row: y});
+    if (tileID !== 3 && tileID !== 4 && tileID !== 7 && tileID !== 8) {
+      this.sprite.playAnimation('walk');
       this.moving = true;
       this.x = x;
       this.y = y;
