439b7ce881eebf7e9dbcac08adb09236708b60dd
kosinaz <kosinaz@gmail.com>
2020-08-28 10:53:18 +0200

    add map

--- src/game.js
+++ src/game.js
@@ -1,55 +1,80 @@
-import { 
+import {
   init,
   Sprite,
   GameLoop,
   initKeys,
   bindKeys,
-  keyPressed
-} from '/lib/kontra.min.mjs'
+  keyPressed,
+  load,
+  TileEngine,
+  dataAssets,
+} from '/lib/kontra.min.mjs';
 
-let { canvas } = init();
+/**
+ * Represents the game.
+ *
+ * @class Game
+ */
+class Game {
+  /**
+   * Creates an instance of Game.
+   * @memberof Game
+   */
+  constructor() {
+    const {canvas} = init();
 
-canvas.width = screen.availWidth;
-canvas.height = screen.availHeight;
+    canvas.width = screen.availWidth;
+    canvas.height = screen.availHeight;
 
-initKeys();
+    initKeys();
 
-bindKeys(['left', 'right', 'up', 'down'], (e) => {
-  e.preventDefault();
-});
+    bindKeys(['left', 'right', 'up', 'down'], (e) => {
+      e.preventDefault();
+    });
 
-let sprite = Sprite({
-  x: 100,        // starting x,y position of the sprite
-  y: 80,
-  color: 'red',  // fill color of the sprite rectangle
-  width: 20,     // width and height of the sprite rectangle
-  height: 40,
-});
+    let map;
 
-let loop = GameLoop({  // create the main game loop
-  update: () => { // update the game state
-    if (keyPressed('left')) {
-      // left arrow pressed
-      sprite.x -= 10;
-    }
-    else if (keyPressed('right')) {
-      // right arrow pressed
-      sprite.x += 10;
-    }
+    load('image/tileset.png', 'data/map.json').then((assets) => {
+      // eslint-disable-next-line new-cap
+      map = TileEngine(dataAssets['data/map']);
+      loop.start(); // start the game
+    });
 
-    if (keyPressed('up')) {
-      // up arrow pressed
-      sprite.y -= 10;
-    }
-    else if (keyPressed('down')) {
-      // down arrow pressed
-      sprite.y += 10;
-    }
-    sprite.update();
-  },
-  render: () => { // render the game state
-    sprite.render();
+    // eslint-disable-next-line new-cap
+    const sprite = Sprite({
+      x: 100, // starting x,y position of the sprite
+      y: 80,
+      color: 'red', // fill color of the sprite rectangle
+      width: 20, // width and height of the sprite rectangle
+      height: 40,
+    });
+
+    // eslint-disable-next-line new-cap
+    const loop = GameLoop({ // create the main game loop
+      update: () => { // update the game state
+        if (keyPressed('left')) {
+          // left arrow pressed
+          sprite.x -= 10;
+        } else if (keyPressed('right')) {
+          // right arrow pressed
+          sprite.x += 10;
+        }
+
+        if (keyPressed('up')) {
+          // up arrow pressed
+          sprite.y -= 10;
+        } else if (keyPressed('down')) {
+          // down arrow pressed
+          sprite.y += 10;
+        }
+        sprite.update();
+      },
+      render: () => { // render the game state
+        map.render();
+        sprite.render();
+      },
+    });
   }
-});
+}
 
-loop.start();    // start the game
+new Game();
